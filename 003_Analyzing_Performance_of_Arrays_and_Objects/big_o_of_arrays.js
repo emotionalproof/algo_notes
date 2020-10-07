f2a3ff76e91a071e7fa0c0b8edec98b76be58cf6
emotionalproof <chaserabenn@gmail.com>
2020-10-06 22:38:38 -0400

    notes

---
+++ 003_Analyzing_Performance_of_Arrays_and_Objects/big_o_of_arrays.js
@@ -0,0 +1,80 @@
+//ARRAYS
+
+//ordered lists
+
+let names = ["Michael", "Melissa", "Andrea"]
+let values = [true, {}, [], 2, "awesome"]
+
+//when to use?
+    //when you need order
+    // when you need fast access/insertion and removal (sort of . . .)
+
+//Big O of Arrays
+// insertion - it depends . . . 
+    //depends on where we are inserting
+    //array.push is O(1), just adding to end of the array and creating new index
+    //if at the beginning O(n)
+// removal - it depends . . . 
+    //same as inserting
+// searching - O(n)
+    //iterate through and check until find target element
+// access - O(1)
+    //array[1]
+
+
+//Built in array methods
+//push O(1)
+//pop 0(1)
+//shift/unshift O(n)
+//concat O(n)
+    //array1.concat(array2)
+    // technically O(m + n)
+//slice O(n)
+    //returns a shallow copy of a portion of an array into a new array object
+        // selected from begin to end (end not included)
+        //original array will not be modified
+        //let animals = [0, 1, 2, 3, 4]
+        // animals.slice(2) //=> [2, 3, 4]
+        // animals.slice(2, 4) //=> [2, 3]
+        // animals.slice(1, 5) //=> [1, 2, 3, 4]
+//splice O(n)
+    //changes the contents of an array by removing existing elements and/or adding new elements
+    //let months = ['Jan', 'March', 'April', 'June']
+    //
+//sort )(n log n)
+//forEach/map/filter/reduce/etc - O(n)
+
+
+
+//two mechinisms for storing data on computer
+//RAM(memory)
+//Storage
+
+//data on storage is permanent
+//RAM(memory) is temporary
+
+//reading and writing data from storage is slow
+//memory is quicker
+
+//applications are stored in storage
+    //but loads into memory
+
+let hash = {
+    4:1,
+    1:5
+    }
+
+const findKeyMaxValue = obj => {
+    let maxKey = 0
+    let maxValue = 0
+    let keys = Object.keys(obj)
+    for (let i = 0; i < keys.length; i++) {
+        if (obj[keys[i]] > maxValue) {
+            maxKey = keys[i]
+            maxValue = obj[keys[i]]
+        }
+    }
+    return maxKey
+}
+
+findKeyMaxValue(hash)
