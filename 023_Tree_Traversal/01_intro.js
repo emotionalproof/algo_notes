14ac513728f231b4c5627e5f8443f2c23aa5f963
emotionalproof <chaserabenn@gmail.com>
2021-06-16 16:58:37 -0400

    update

--- 023_Tree_Traversal/01_intro.js
+++ 023_Tree_Traversal/01_intro.js
@@ -24,19 +24,37 @@
 //two main approaches for traversing a tree
 //Breadth-first Search
 //Depth-first search 
-//refer to the directions 
+//refer to the general directions 
 
 
 //                     (10)
 //                     /   \
-//                  (6)    (20)
-//
-// 
+//                  (6)    (15)
+//                  / \      \
+//               (3)   (8)   (20)
 
 //BFS
+//  ----------------->  (10)
+//                     /   \
+//  --------------> (6) -->(15)
+//                  / \      \
+//  -----------> (3)->(8)--->(20)
 //think about moving across the tree levels/siblings
 //[root, root.left, root.right, root.left.left, root.left.right, root.right.right]
+//[10, 6, 15, 3, 8, 20]
+
+
 //Depth-first Search or DFS
+//                     (10)
+//                     /   \
+//                  (6)    (15)
+//                  / \      \
+//               (3)   (8)   (20)
     //three main orders
     //primary direction is down and then going back up 
     //InOrder
+        // [3, 6, 8, 10, 15, 20]
+    //PreOrder
+        //[10, 6, 3, 8, 15, 20]
+    //PostOrder
+        //[3, 8, 6, 20, 15, 10]
